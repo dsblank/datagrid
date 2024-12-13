a5b63c90f5e8cadeb8ca7ebf0801bbd9fb88d502
Doug Blank <doug.blank@gmail.com>
2024-12-13 11:50:24 -0500

    Version 1.0.8

--- datagrid/_version.py
+++ datagrid/_version.py
@@ -11,5 +11,5 @@
 #    All rights reserved                             #
 ######################################################
 
-version_info = (1, 0, 7)
+version_info = (1, 0, 8)
 __version__ = ".".join(map(str, version_info))
